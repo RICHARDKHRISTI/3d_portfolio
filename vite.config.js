90868dc657c0182e5c09e680a04e4200f4bb9eae
Richard's Legacy <144961180+RICHARDKHRISTI@users.noreply.github.com>
2025-07-12 02:04:46 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://visionary-axolotl-5af3e5.netlify.app/',
+  base: '/3d_portfolio/',
   plugins: [react(), tailwindcss()],
 });
